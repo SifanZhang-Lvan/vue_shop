ae209eeb6a453faf1a7b176f126d4761c0c10cb8
Sifan.Zhang <sifan.zhang@amh-group.com>
2021-06-11 19:04:23 +0800

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
+  <div class="box">
+    <el-container class="home-container">
+      <!-- 头部区域 -->
+      <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">Aside</el-aside>
+        <!-- 右侧内容主体 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    logout () {
+    logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     }
@@ -15,5 +24,24 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+.box {
+  height: 100%;
+}
+
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #EAEDF1;
+}
 </style>
