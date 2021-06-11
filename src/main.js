c905e6e15b310d82a86b526edb8f8dc8465f09a3
Sifan.Zhang <sifan.zhang@amh-group.com>
2021-06-11 18:11:00 +0800

    complete login

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import './css/global.css'
+import './plugins/element.js'
+import axios from 'axios'
+
+// 配置请求根路径
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
